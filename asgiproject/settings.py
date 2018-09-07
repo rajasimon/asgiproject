453b60dad50206df7551f81c5d77b7bb1b9f1d38
Raja Simon <rajasimon@icloud.com>
2018-09-07 12:18:54 +0530

    Update template design

--- asgiproject/settings.py
+++ asgiproject/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 ASGI_APPLICATION = "asgiproject.routing.application"
